fbea01b0268e5d256f7619cec858e611337e665b
timtyree <tttyree@ucsd.edu>
2022-09-08 17:36:19 -0700

    added to doc and fig and etc.

---
+++ notebooks/lib/get_init.py
@@ -0,0 +1,7 @@
+# get_init.py
+#Programmer: Tim Tyree
+#Date: 9.8.2022
+import numpy as np, pandas as pd, matplotlib.pyplot as plt, sys, os
+import time
+from xgboost import XGBClassifier
+from . import *
